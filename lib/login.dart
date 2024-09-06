00a6096c914b4019d225d86635d2219c076e2025
Jeetin9410 <kumaranand8126@gmail.com>
2024-09-06 13:20:06 +0530

    feat: login screen with text controller and navigator

--- lib/login.dart
+++ lib/login.dart
@@ -25,6 +25,8 @@ class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
   @override
   Widget build(BuildContext context) {
+    final _usernameController = TextEditingController();
+    final _passwordController = TextEditingController();
     return Scaffold(
       body: SafeArea(
         child: ListView(
@@ -36,12 +38,46 @@ class _LoginPageState extends State<LoginPage> {
                 Image.asset('assets/diamond.png'),
                 const SizedBox(height: 16.0),
                 const Text('SHRINE'),
+                const SizedBox(height: 100.0),
+                const Text('LOGIN', style: TextStyle(fontSize: 24.0),),
+                const SizedBox(height: 50.0),
+                TextField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                    labelText: "Username",
+                      filled: true
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                      labelText: "Password",
+                    filled: true
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 10.0),
+                OverflowBar(
+                  alignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text('CANCEL'),
+                      onPressed: () {
+                        _usernameController.clear();
+                        _passwordController.clear();
+                      },
+                    ),
+                    ElevatedButton(
+                      child: const Text('NEXT'),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                )
               ],
             ),
-            const SizedBox(height: 120.0),
-            // TODO: Remove filled: true values (103)
-            // TODO: Add TextField widgets (101)
-            // TODO: Add button bar (101)
           ],
         ),
       ),
